35d973ce6ae3319c809a0ddce69c9ae5a0f15353
Ivan Gerasimenko <66555504+evangera@users.noreply.github.com>
2020-07-04 15:08:22 +0300

    Add files via upload

--- calc.js
+++ calc.js
@@ -7,7 +7,12 @@ function plus() {
     num2 = parseInt(num2);
 
     result = num1+num2;
-    document.getElementById('out').innerHTML = result;
+    document.getElementById('out').value = result;
+}
+function result() {
+    plus();
+    plus2();
+    plus3();
 }
 
 function plus2() {
@@ -19,7 +24,7 @@ function plus2() {
     num2 = parseInt(num2);
 
     result = (num1+num2)*1.13;
-    document.getElementById('out').innerHTML = result;
+    document.getElementById('out2').value = result;
 }
 
 function plus3() {
@@ -31,39 +36,62 @@ function plus3() {
     num2 = parseInt(num2);
 
     result = (num1+num2)*1.17;
-    document.getElementById('out').innerHTML = result;
+    document.getElementById('out3').value = result;
 }
 function multiply10() {
-    var num1;
+    var num1, num2;
     num1 = document.getElementById('n1').value;
     num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
 
     result = num1*10;
     document.getElementById('n1').value = result;
+    document.getElementById('out').value = result+num2;
+    document.getElementById('out2').value = (result+num2)*1.13;
+    document.getElementById('out3').value = (result+num2)*1.17;
 }
 function multiply100() {
-    var num1;
+    debugger;
+    var num1, num2;
     num1 = document.getElementById('n1').value;
     num1 = parseInt(num1);
 
-    result = num1*100;
-    document.getElementById('n1').value = result;
-}
-function multiply2() {
-    var num2;
     num2 = document.getElementById('n2').value;
     num2 = parseInt(num2);
+
+    result = num1*100;
+    document.getElementById('n1').value = result;
+    document.getElementById('out').value = result+num2;
+    document.getElementById('out2').value = (result+num2)*1.13;
+    document.getElementById('out3').value = (result+num2)*1.17;
+}
+function multiply2() {
+    var num2, num1;
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
 
     result = num2*2;
     document.getElementById('n2').value = result;
+    document.getElementById('out').value = result+num1;
+    document.getElementById('out2').value = (result+num1)*1.13;
+    document.getElementById('out3').value = (result+num1)*1.17;
 }
 function multiply4() {
-    var num2;
+    var num2, num1;
     num2 = document.getElementById('n2').value;
     num2 = parseInt(num2);
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
 
     result = num2*4;
     document.getElementById('n2').value = result;
+    document.getElementById('out').value = result+num1;
+    document.getElementById('out2').value = (result+num1)*1.13;
+    document.getElementById('out3').value = (result+num1)*1.17;
 }
 function clean() {
     var num1, num2;
@@ -73,4 +101,7 @@ function clean() {
     document.getElementById('n2').value = ""
     num2 = parseInt(num2);
 
+    document.getElementById('out').value = ""
+    document.getElementById('out2').value = ""
+    document.getElementById('out3').value = ""
 }
